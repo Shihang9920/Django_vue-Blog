57d8a89da28fc671a343fcaab618838a1a8c7482
Shihang <shihang9920@163.com>
2023-03-24 15:35:53 +0800

    修改

--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,1 @@
-# from django.urls import path
-# from article import views
-#
-# app_name = 'article'
-# urlpatterns = [
-#     path('', views.ArticleViewSet.as_view(), name='list'),  # name参数可以反向解析url
-#     path('<int:pk>/', views.ArticleViewSet.as_view(), name='detail')
-# ]
+
